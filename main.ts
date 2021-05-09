6fad5dac1b3365eee00128bb1630050b44c80c0e
Alan_Chang <namssob@gmail.com>
2021-05-09 20:50:55 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,45 +1,76 @@
 let 等待時間 = 0
 input.onButtonPressed(Button.A, function () {
-    等待時間 = 2000
+    ezstartkit.oled_clear()
+    ezstartkit.oled_init()
+    ezstartkit.oled_showString(0, 0, "Button A")
     led.enable(true)
+    music.setBuiltInSpeakerEnabled(false)
+    等待時間 = 500
+    music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.OnceInBackground)
     images.createImage(`
-        # . . . #
-        # . . . #
+        # # . # #
         # # # # #
-        # . . . #
-        # . . . #
-        `).showImage(0, 等待時間)
+        # # # # #
+        . # # # .
+        . . # . .
+        `).scrollImage(1, 等待時間)
     images.createImage(`
-        # # # # #
-        # . . . .
-        # # # # #
-        # . . . .
-        # # # # #
-        `).showImage(0, 等待時間)
+        . # . # .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # . # .
+        `).scrollImage(1, 200)
     images.createImage(`
-        # . . . .
-        # . . . .
-        # . . . .
-        # . . . .
-        # # # # #
-        `).showImage(0, 等待時間)
+        . . # . .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # . # .
+        `).scrollImage(1, 200)
     images.createImage(`
-        # # . . .
-        # # . . .
-        # # . . .
-        # # # # #
-        # # # # #
-        `).showImage(0, 等待時間)
+        . # # . .
+        . # . # .
+        . # # # .
+        . # . . .
+        . # . . .
+        `).scrollImage(1, 200)
     images.createImage(`
+        . # # . .
+        . # . # .
+        . # # # .
+        . # . . .
+        . # . . .
+        `).scrollImage(1, 200)
+    images.createImage(`
+        . # . # .
+        . # . # .
+        . . # . .
+        . . # . .
+        . . # . .
+        `).scrollImage(1, 200)
+    images.createImage(`
+        . . . . .
+        . # # # .
+        . # # # .
         # # # # #
-        # . . . #
-        # . . . #
-        # . . . #
         # # # # #
-        `).showImage(0, 等待時間)
-    if (true) {
-    	
-    }
+        `).scrollImage(1, 200)
+    images.createImage(`
+        # # . # #
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `).showImage(0)
+})
+input.onGesture(Gesture.Shake, function () {
+    led.enable(false)
+    music.setBuiltInSpeakerEnabled(false)
+})
+input.onButtonPressed(Button.B, function () {
+    led.enable(true)
+    music.setBuiltInSpeakerEnabled(true)
 })
 basic.forever(function () {
 	
